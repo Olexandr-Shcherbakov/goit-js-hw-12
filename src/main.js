cf220d59f7bcf55fab657a9b09482afb4a726fed
Olexandr Shch <o73473708@gmail.com>
2025-08-17 16:13:05 +0300

    hwjs12p3

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import {
 } from './js/render-functions.js';
 
 
-const form = document.querySelector('.search-form');
+const form = document.querySelector('.form');
 const loadMoreBtn = document.querySelector('.load-more');
 
 let query = '';
